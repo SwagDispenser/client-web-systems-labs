b37f11348ec6ad3840106de53a66dd8a3c17a90f
dmytro <dimadovgesh@gmail.com>
2026-09-13 22:40:20 +0300

    complete first task

---
+++ lab-01 /src/variables.ts
@@ -0,0 +1,14 @@
+const myName: string = "Dmytro"
+let myAge: number = 18
+let studying: boolean = true
+
+let friendNames: string[] = ["Jeka", "Lyonia", "IvanBossauto"]
+const friendAges: number[] = [19,19,19]
+let friendStatus: any = "Alive"
+
+console.log(myName)
+console.log(myAge)
+console.log(studying)
+console.log(friendNames)
+console.log(friendAges)
+console.log(friendStatus)
